620b33acc899cec0b989579f772d5c8c09f04b05
AlexPr0ject <services.alex@outlook.com>
2024-11-15 22:21:38 -0500

    Ajusts

--- cisfront/src/app/modules/cis/cis-routing.module.ts
+++ cisfront/src/app/modules/cis/cis-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IndexComponent } from './index/index.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {path: 'index', component: IndexComponent},
   {path: 'page-not-found', component: PageNotFoundComponent},
+  {path: 'login', component: LoginComponent},
 ];
 
 @NgModule({
--- cisfront/src/app/modules/cis/index/index.component.ts
+++ cisfront/src/app/modules/cis/index/index.component.ts
@@ -24,6 +24,6 @@ export class IndexComponent {
    }
 
    login() {
-    this.router.navigate(['/authentication/login'])
+    this.router.navigate(['/cis/login'])
    }
 }
